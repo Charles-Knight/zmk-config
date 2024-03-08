875849a379b386fdbc156210670f063b87159e1d
Charles Knight <Charles-Knight@users.noreply.github.com>
2024-03-07 23:01:49 -0800

    Changed image to img

--- config/boards/shields/zaphod_lite/zaphod_status_screen.c
+++ config/boards/shields/zaphod_lite/zaphod_status_screen.c
@@ -32,7 +32,7 @@ lv_obj_t *zmk_display_status_screen() {
 
     // Create Image
 
-    lv_obj_t *img = lv_image_create(screen);
+    lv_obj_t *img = lv_img_create(screen);
     lv_image_set_src(img, LV_SYMBOL_OK "Accept");
     lv_obj_align(img, LV_ALIGN_CENTER, 0, 0);
 
