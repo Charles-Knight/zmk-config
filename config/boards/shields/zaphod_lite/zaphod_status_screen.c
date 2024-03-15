05f87ba4ad6a9d836ec1a2d3ba77ba00c87cc036
Charles Knight <Charles-Knight@users.noreply.github.com>
2024-03-14 22:12:21 -0700

    Changed lv_scr_act() to screen

--- config/boards/shields/zaphod_lite/zaphod_status_screen.c
+++ config/boards/shields/zaphod_lite/zaphod_status_screen.c
@@ -34,7 +34,7 @@ lv_obj_t *zmk_display_status_screen() {
 
     // Create image with custom image 
     
-    lv_obj_t * img1 = lv_img_create(lv_scr_act());
+    lv_obj_t * img1 = lv_img_create(screen);
     lv_img_set_src(img1, &mountain);
     lv_obj_set_size(img1, 160, 68);
     lv_obj_align(img1, LV_ALIGN_CENTER, 0, 0);
